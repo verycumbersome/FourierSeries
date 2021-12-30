5936b952a00fea99a88a6dde8632bf6369bee707
Matthew Jordan <matthewsages@gmail.com>
2021-12-30 12:11:21 -0800

    Add initial equaiton

--- main.py
+++ main.py
@@ -1,10 +1,45 @@
+import math
+
 import numpy as np
 import scipy.integrate as integrate
 import matplotlib.pyplot as plt
 
+fig = plt.figure()
+# ax = plt.axes(projection='3d')
 
-def main():
-    pass
+
+def func_orig(x):
+    out = math.cos(523 * x)
+    out += math.cos(440 * x)
+    out += math.cos(349 * x)
+
+    return out
+
+
+def func_prime(x):
+    out = -math.sin(x)
+
+    return out
+
+
+def display_func(function):
+    x = np.linspace(0, 20, 100)
+    y = np.linspace(0, 20, 100)
+    grid = np.meshgrid(x, y)
+
+    # Map function across numpy array 
+    out = np.vectorize(function)(x)
+
+    plt.plot(out)
+
+    return out
 
 if __name__=="__main__":
-    main()
+    display_func(func_orig)
+    # display_func(func_prime)
+
+    plt.show(block=False)
+    plt.pause(0.001)
+    input("hit[enter] to end.")
+    plt.close('all')
+
