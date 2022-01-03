b27ce9b4c3172f816b15a029a867955ad0480db3
Matthew Jordan <matthewsages@gmail.com>
2022-01-03 04:12:29 -0800

    Add seaborn

--- main.py
+++ main.py
@@ -3,51 +3,48 @@ import math
 import numpy as np
 import scipy.integrate as integrate
 import matplotlib.pyplot as plt
+import seaborn as sns
 
-fig = plt.figure()
+fig, (ax1, ax2) = plt.subplots(2)
+fig.suptitle('Vertically stacked subplots')
 # ax = plt.axes(projection='3d')
 
-
-def func_orig(x):
-    out = math.cos(523 * x)
-    out += math.cos(440 * x)
-    out += math.cos(349 * x)
-
-    return out
+# freqs = [523, 440, 349]
+freqs = [1, 4, 15]
 
 
-def func_prime(x):
-    out = -math.sin(x)
+def func(x):
+    out = sum([math.sin(f * x) for f in freqs])
 
     return out
 
 
-def fourier_transform(func, f, t):
+def fourier_transform(w):
     # x(t) * e^-i2(pi)ft
-    out = func(t) * math.e ** (2 * math.pi * f * t)
-
-    integrate.quad(f1, np.inf, -np.inf)
+    fourier = lambda t: (func(t) * (math.e ** (-1j * w * t))).imag
+    out = integrate.quad(fourier, -np.inf, np.inf)[0]
 
     return out
 
 
 def display_func(func):
     x = np.linspace(0, 20, 100)
-    t = np.linspace(-1, 1, 100)
+    f = np.linspace(0, 30, 100)
 
     # Map function across numpy array 
     out = np.vectorize(func)(x)
-    fourier = np.vectorize(fourier_transform)(func, 2, t)
+    fourier = np.vectorize(fourier_transform)(f)
 
-    # plt.plot(out)
-    plt.plot(fourier)
+    sns.lineplot(x=x, y=out, ax=ax1)
+    sns.lineplot(x=f, y=fourier, ax=ax2)
 
 
 if __name__=="__main__":
-    display_func(func_orig)
+    display_func(func)
     # display_func(func_prime)
 
-    plt.show(block=False)
+    plt.autoscale()
+    plt.show(block=True)
     plt.pause(0.001)
     input("hit[enter] to end.")
     plt.close('all')
